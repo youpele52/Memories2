c0bb2b3f437365b0fb172ea38565bba49f03d815
Youpele Michael <mjayjesus@gmail.com>
2021-06-14 11:26:42 +0200

    Update Movies.js
    Made it possible for the movies cards to take more to take up more columns in screens from 2000px upwards

--- components/Movies.js
+++ components/Movies.js
@@ -6,10 +6,10 @@ import FlipMove from 'react-flip-move'
 function Movies({ movies }) {
   //   console.log(movies)
   return (
-    // 3x: is not part of default tailwindcss, i define it myself under
+    // 3xl: is not part of default tailwindcss, i define it myself under
     // tailwind.config.js file undertheme: {extend: {
-    // in 3x: ie in larger screens say 4k screens the page renders with flexbox instead of css grid
-    <FlipMove className='px-5 my-10 sm:grid md:grid-cols-2 xl:grid-cols-3  3x:flex flex-wrap justify-center'>
+    // in 3xl: ie in larger screens say 4k screens the page renders with flexbox instead of css grid
+    <FlipMove className='px-5 my-10 sm:grid md:grid-cols-2 xl:grid-cols-3  3xl:flex flex-wrap justify-center'>
       {movies.map((movie) => (
         <Movie key={movie.id} movie={movie} />
       ))}
